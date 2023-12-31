1ef3d38bf2a7ce31f61c252d400484b5d75bdd03
kazakeviciute <ingaa.kazakeviciute@gmail.com>
2023-12-31 12:14:44 +0200

    frontend backend sujungimas

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8080
 const fruits = ['apple']
 
